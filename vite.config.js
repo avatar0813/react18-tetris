39e3d247964bab9a8df5de2d7a4eed35ede8bee3
avatar <oooooooyk0813@gmail.com>
2023-05-26 15:44:24 +0800

    feat: 资源版本替换

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssPresetEnv from 'postcss-preset-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), postcssPresetEnv],
 })
+
