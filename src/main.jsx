0237edfa5d0526636c86d182a112a99479b90e43
avatar <oooooooyk0813@gmail.com>
2023-05-26 16:43:15 +0800

    feat: 文件处理

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './containers/'
-import './index.css'
 import { Provider } from 'react-redux'
 import store from './store'
 
